53a20bd0084ba691af758054e350bfdd43fde824
manoj515876 <manojputtaluckey@gmail.com>
2026-06-19 12:05:25 +0530

    Data types in shell script

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -6,3 +6,11 @@ NUM2=manojmantha
 SUM=$(($NUM1+$NUM2))
 
 echo "Sum is: $SUM"
+
+# Array
+FRUITS=("Apple" "Banana" "Pomo")
+
+echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
