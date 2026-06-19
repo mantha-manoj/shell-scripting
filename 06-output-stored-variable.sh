d9f9593b704d4b0b411fefc0680c31c0eb31654d
manoj515876 <manojputtaluckey@gmail.com>
2026-06-19 11:52:47 +0530

    Storing date output in variable and calculate how many seconds to take execute script

---
+++ 06-output-stored-variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+START_TIME=$(data +%s)
+
+echo "Script executed at: $START_TIME"
+
+sleep 10
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($START_TIME-$END_TIME))
+
+echo "Script executed in: $TOTAL_TIME seconds"
