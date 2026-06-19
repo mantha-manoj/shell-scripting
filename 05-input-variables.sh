970aedbe37a21acbecfc1f619b848dabd887ed85
manoj515876 <manojputtaluckey@gmail.com>
2026-06-19 11:49:33 +0530

    Reading input from user like username and password

---
+++ 05-input-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter your username : "
+read USERNAME
+
+echo "Please enter your password : "
+read -s PASSWORD
+
+echo "Your username is : $USERNAME"
+echo "Your password is : $PASSWORD"
