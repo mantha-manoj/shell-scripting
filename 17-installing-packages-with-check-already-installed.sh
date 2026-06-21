045cf0303a8bc2bebef92fa4bb9347a41becaf59
manoj515876 <manojputtaluckey@gmail.com>
2026-06-21 14:17:26 +0530

    Installing packages with check if already installed or not

---
+++ 17-installing-packages-with-check-already-installed.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Run using this command sudo sh 16-installing-package-with-for-loop.sh mysql nginx nodejs
+
+USERID=$(id -u) # check the root user or not
+LOG_FOLDER="/var/log/shell-script"
+LOG_FILE="$LOG_FOLDER/$0.log"
+
+mkdir -p $LOG_FOLDER
+
+if [ $USERID -ne 0 ]; then 
+    echo "Please run this script with root user access" | tee -a $LOG_FILE
+    exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then 
+        echo "$2 ... FAILURE" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo "$2 ... SUCCESS" | tee -a $LOG_FILE
+    fi
+}
+
+for package in $@
+do
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now"
+        dnf install $package -y &>>$LOGS_FILE
+        VALIDATE $? "$package installation"
+    else
+        echo "$package already installed, skipping"
+    fi
+done
+
