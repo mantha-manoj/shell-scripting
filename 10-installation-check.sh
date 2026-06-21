4d1976bf49d4decb398751c385224fdece27a949
manoj515876 <manojputtaluckey@gmail.com>
2026-06-21 13:07:43 +0530

    Installing nginx, mysql and nodejs

--- 10-installation-check.sh
+++ 10-installation-check.sh
@@ -16,3 +16,23 @@ if [ $? -ne 0 ]; then
 else
     echo "Installing Nginx ... SUCCESS"
 fi
+
+echo "Installing mysql"
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then 
+    echo "Installing MySQL ... FAILURE"
+    exit 1
+else
+    echo "Installing MySQL ... SUCCESS"
+fi
+
+echo "Installing nodejs"
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then 
+    echo "Installing Nodejs ... FAILURE"
+    exit 1
+else
+    echo "Installing Nodejs ... SUCCESS"
+fi
